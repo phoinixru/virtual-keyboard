de5a6e1b604722b1ed909c7469680f320b9fb7e2
Vitaly Kukushkin <phoinixru@gmail.com>
2023-04-28 18:11:52 +0200

    refactor: cleanup code

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,243 +1,149 @@
 export default {
   /* Alphanumeric Section */
   Digit0: {
-    keys: {
-      en: ['0', ')'], ru: ['0', ')'],
-    },
+    keys: { en: ['0', ')'], ru: ['0', ')'] },
   },
   Digit1: {
-    keys: {
-      en: ['1', '!'], ru: ['1', '!'],
-    },
+    keys: { en: ['1', '!'], ru: ['1', '!'] },
   },
   Digit2: {
-    keys: {
-      en: ['2', '@'], ru: ['2', '"'],
-    },
+    keys: { en: ['2', '@'], ru: ['2', '"'] },
   },
   Digit3: {
-    keys: {
-      en: ['3', '#'], ru: ['3', '№'],
-    },
+    keys: { en: ['3', '#'], ru: ['3', '№'] },
   },
   Digit4: {
-    keys: {
-      en: ['4', '$'], ru: ['4', '%'],
-    },
+    keys: { en: ['4', '$'], ru: ['4', '%'] },
   },
   Digit5: {
-    keys: {
-      en: ['5', '%'], ru: ['5', ':'],
-    },
+    keys: { en: ['5', '%'], ru: ['5', ':'] },
   },
   Digit6: {
-    keys: {
-      en: ['6', '^'], ru: ['6', ','],
-    },
+    keys: { en: ['6', '^'], ru: ['6', ','] },
   },
   Digit7: {
-    keys: {
-      en: ['7', '&'], ru: ['7', '.'],
-    },
+    keys: { en: ['7', '&'], ru: ['7', '.'] },
   },
   Digit8: {
-    keys: {
-      en: ['8', '*'], ru: ['8', ';'],
-    },
+    keys: { en: ['8', '*'], ru: ['8', ';'] },
   },
   Digit9: {
-    keys: {
-      en: ['9', '('], ru: ['9', '('],
-    },
+    keys: { en: ['9', '('], ru: ['9', '('] },
   },
 
   /* Letters */
   KeyQ: {
-    keys: {
-      en: ['q', 'Q'], ru: ['й', 'Й'],
-    },
+    keys: { en: ['q', 'Q'], ru: ['й', 'Й'] },
   },
   KeyW: {
-    keys: {
-      en: ['w', 'W'], ru: ['ц', 'Ц'],
-    },
+    keys: { en: ['w', 'W'], ru: ['ц', 'Ц'] },
   },
   KeyE: {
-    keys: {
-      en: ['e', 'E'], ru: ['у', 'У'],
-    },
+    keys: { en: ['e', 'E'], ru: ['у', 'У'] },
   },
   KeyR: {
-    keys: {
-      en: ['r', 'R'], ru: ['к', 'К'],
-    },
+    keys: { en: ['r', 'R'], ru: ['к', 'К'] },
   },
   KeyT: {
-    keys: {
-      en: ['t', 'T'], ru: ['е', 'Е'],
-    },
+    keys: { en: ['t', 'T'], ru: ['е', 'Е'] },
   },
   KeyY: {
-    keys: {
-      en: ['y', 'Y'], ru: ['н', 'Н'],
-    },
+    keys: { en: ['y', 'Y'], ru: ['н', 'Н'] },
   },
   KeyU: {
-    keys: {
-      en: ['u', 'U'], ru: ['г', 'Г'],
-    },
+    keys: { en: ['u', 'U'], ru: ['г', 'Г'] },
   },
   KeyI: {
-    keys: {
-      en: ['i', 'I'], ru: ['ш', 'Ш'],
-    },
+    keys: { en: ['i', 'I'], ru: ['ш', 'Ш'] },
   },
   KeyO: {
-    keys: {
-      en: ['o', 'O'], ru: ['щ', 'Щ'],
-    },
+    keys: { en: ['o', 'O'], ru: ['щ', 'Щ'] },
   },
   KeyP: {
-    keys: {
-      en: ['p', 'P'], ru: ['з', 'З'],
-    },
+    keys: { en: ['p', 'P'], ru: ['з', 'З'] },
   },
   KeyA: {
-    keys: {
-      en: ['a', 'A'], ru: ['ф', 'Ф'],
-    },
+    keys: { en: ['a', 'A'], ru: ['ф', 'Ф'] },
   },
   KeyS: {
-    keys: {
-      en: ['s', 'S'], ru: ['ы', 'Ы'],
-    },
+    keys: { en: ['s', 'S'], ru: ['ы', 'Ы'] },
   },
   KeyD: {
-    keys: {
-      en: ['d', 'D'], ru: ['в', 'В'],
-    },
+    keys: { en: ['d', 'D'], ru: ['в', 'В'] },
   },
   KeyF: {
-    keys: {
-      en: ['f', 'F'], ru: ['а', 'А'],
-    },
+    keys: { en: ['f', 'F'], ru: ['а', 'А'] },
   },
   KeyG: {
-    keys: {
-      en: ['g', 'G'], ru: ['п', 'П'],
-    },
+    keys: { en: ['g', 'G'], ru: ['п', 'П'] },
   },
   KeyH: {
-    keys: {
-      en: ['h', 'H'], ru: ['р', 'Р'],
-    },
+    keys: { en: ['h', 'H'], ru: ['р', 'Р'] },
   },
   KeyJ: {
-    keys: {
-      en: ['j', 'J'], ru: ['о', 'О'],
-    },
+    keys: { en: ['j', 'J'], ru: ['о', 'О'] },
   },
   KeyK: {
-    keys: {
-      en: ['k', 'K'], ru: ['л', 'Л'],
-    },
+    keys: { en: ['k', 'K'], ru: ['л', 'Л'] },
   },
   KeyL: {
-    keys: {
-      en: ['l', 'L'], ru: ['д', 'Д'],
-    },
+    keys: { en: ['l', 'L'], ru: ['д', 'Д'] },
   },
   KeyZ: {
-    keys: {
-      en: ['z', 'Z'], ru: ['я', 'Я'],
-    },
+    keys: { en: ['z', 'Z'], ru: ['я', 'Я'] },
   },
   KeyX: {
-    keys: {
-      en: ['x', 'X'], ru: ['ч', 'Ч'],
-    },
+    keys: { en: ['x', 'X'], ru: ['ч', 'Ч'] },
   },
   KeyC: {
-    keys: {
-      en: ['c', 'C'], ru: ['с', 'С'],
-    },
+    keys: { en: ['c', 'C'], ru: ['с', 'С'] },
   },
   KeyV: {
-    keys: {
-      en: ['v', 'V'], ru: ['м', 'М'],
-    },
+    keys: { en: ['v', 'V'], ru: ['м', 'М'] },
   },
   KeyB: {
-    keys: {
-      en: ['b', 'B'], ru: ['и', 'И'],
-    },
+    keys: { en: ['b', 'B'], ru: ['и', 'И'] },
   },
   KeyN: {
-    keys: {
-      en: ['n', 'N'], ru: ['т', 'Т'],
-    },
+    keys: { en: ['n', 'N'], ru: ['т', 'Т'] },
   },
   KeyM: {
-    keys: {
-      en: ['m', 'M'], ru: ['ь', 'Ь'],
-    },
+    keys: { en: ['m', 'M'], ru: ['ь', 'Ь'] },
   },
 
   /* Punctuation */
   Backquote: {
-    keys: {
-      en: ['`', '~'], ru: [']', '['],
-    },
+    keys: { en: ['`', '~'], ru: [']', '['] },
   },
   Backslash: {
-    keys: {
-      en: ['\\', '|'], ru: ['ё', 'Ё'],
-    },
+    keys: { en: ['\\', '|'], ru: ['ё', 'Ё'] },
   },
   BracketLeft: {
-    keys: {
-      en: ['[', '{'], ru: ['х', 'Х'],
-    },
+    keys: { en: ['[', '{'], ru: ['х', 'Х'] },
   },
   BracketRight: {
-    keys: {
-      en: [']', '}'], ru: ['ъ', 'Ъ'],
-    },
+    keys: { en: [']', '}'], ru: ['ъ', 'Ъ'] },
   },
   Comma: {
-    keys: {
-      en: [',', '<'], ru: ['б', 'Б'],
-    },
+    keys: { en: [',', '<'], ru: ['б', 'Б'] },
   },
   Equal: {
-    keys: {
-      en: ['=', '+'], ru: ['=', '+'],
-    },
+    keys: { en: ['=', '+'], ru: ['=', '+'] },
   },
   Minus: {
-    keys: {
-      en: ['-', '_'], ru: ['-', '_'],
-    },
+    keys: { en: ['-', '_'], ru: ['-', '_'] },
   },
   Period: {
-    keys: {
-      en: ['.', '>'], ru: ['ю', 'Ю'],
-    },
+    keys: { en: ['.', '>'], ru: ['ю', 'Ю'] },
   },
   Quote: {
-    keys: {
-      en: ['\'', '"'], ru: ['э', 'Э'],
-    },
+    keys: { en: ['\'', '"'], ru: ['э', 'Э'] },
   },
   Semicolon: {
-    keys: {
-      en: [';', ':'], ru: ['ж', 'Ж'],
-    },
+    keys: { en: [';', ':'], ru: ['ж', 'Ж'] },
   },
   Slash: {
-    keys: {
-      en: ['/', '?'], ru: ['/', '?'],
-    },
+    keys: { en: ['/', '?'], ru: ['/', '?'] },
   },
 
   /* Functional Keys */
@@ -253,7 +159,7 @@ export default {
     icon: '⌥',
     isFunctional: true,
   },
-    Backspace: {
+  Backspace: {
     key: 'Backspace',
     label: 'backspace',
     icon: '⌫',
